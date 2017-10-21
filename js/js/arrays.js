04381f4263c40387e79ba5e060560f0be4190e3a
Maciej Mucha <mac.muc@wp.pl>
2017-10-21 11:40:24 +0200

    tablice i obiekty

--- js/js/arrays.js
+++ js/js/arrays.js
@@ -11,3 +11,24 @@ console.log(imiona);
 var ileElementow = imiona.push('Geralt');
 
 console.log(imiona);
+
+console.log(ileElementow);
+
+imiona.pop();
+
+console.log(imiona);
+
+var zdjetyElement = imiona.pop();
+
+console.log(zdjetyElement);
+
+console.log('Ile elementów w tablicy: ' + imiona.length);
+
+console.log(imiona.join(' - '));
+
+imiona.reverse();
+console.log(imiona);
+
+imiona.sort();
+console.log(imiona);
+
---
+++ js/js/objects.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var osoba = {
+    imie : 'Wojtek',
+    wzrost: 181,
+    przedstawOsobe:function() {console.log(this.imie)};
+    
+}
+
+console.log(osoba.imie);
+console.log(osoba['imie']);
+
+osoba.przedstawOsobe();
+
+osoba.nazwisko = 'Potocki';
+
+console.log(osoba.nazwisko);
+
+console.log(osoba);
