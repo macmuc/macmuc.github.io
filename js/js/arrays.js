50141bc43d5f54f12ded7e2b1d17dc7f27ef831e
Maciej Mucha <mac.muc@wp.pl>
2017-10-21 10:57:13 +0200

    plik index.html i pliki js

---
+++ js/js/arrays.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var imiona= ['Wojtek', 'Krystian', 'Łukasz'];
+
+console.log(imiona[0]);
+
+imiona[3]= 'Piotrek';
+
+console.log(imiona);
+
+var ileElementow = imiona.push('Geralt');
+
+console.log(imiona);
---
+++ js/js/datatypes.js
@@ -0,0 +1,27 @@
+'use strict';
+/*typ liczbowy*/
+
+var premia = 1000;
+var wyplata = 5000;
+
+console.log(wyplata + premia);
+
+/* Typ - łańcuch znaków - string */
+
+var premiaStr = "2500";
+var wyplataStr = "5000";
+
+console.log(wyplataStr + premiaStr); // + to operator konkatenacji, sluży do łaczenia stringów
+
+/* typ logiczny */
+
+var czyJestSmog = true;
+
+console.log(czyJestSmog);
+
+/*Typy specjalne - undefined / null*/
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null; 
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
