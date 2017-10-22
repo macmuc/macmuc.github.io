83aa8ad813eca4ca39b3524b665c8a750d300d03
Maciej Mucha <mac.muc@wp.pl>
2017-10-22 10:16:42 +0200

    znajdowanie elementów w js

--- DOM/js/znajdowanie_elementow.js
+++ DOM/js/znajdowanie_elementow.js
@@ -40,3 +40,44 @@ console.log( document.getElementById( "parFirst" ).childNodes );
 console.log( document.getElementById( "parFirst" ).childNodes[1] );
 console.log( document.getElementById( "parFirst" ).firstChild );
 console.log( document.getElementById( "parFirst" ).lastChild ); 
+
+var istniejacyWezel = document.getElementById('parSecond');
+console.log(istniejacyWezel);
+
+var newElement = document.createElement('p');
+
+var newElementContent = document.createTextNode('To jest nowy paragraf');
+
+newElement.appendChild(newElementContent);
+
+console.log(newElement);
+
+istniejacyWezel.appendChild(newElement);
+istniejacyWezel.removeChild(newElement);
+
+/*var parFirstDiv = document.getElementById('parFirst');
+var linkWParagrafie = document.getElementById('parFirst').children[2];
+console.log(linkWParagrafie);
+parFirstDiv.replaceChild(newElement,linkWParagrafie);*/
+
+//Dodaj znacznik <br> po każdym linku i usuń z nich atrybut klasy
+
+
+var allLinks = document.getElementsByTagName('a');
+//console.log(allLinks);
+
+//i = index zaczyna się od zera elementów jest sześć, więc musi być mniejszy od 6, kończy się na pięć
+
+
+    
+   for (var i = 0; i < allLinks.length; i++){
+    var br = document.createElement ('br');
+    allLinks[i].parentNode.insertBefore(br,
+    allLinks[i].nextSibling);
+    allLinks[i].removeAttribute('class');//usuwa atrybut klasy
+    allLinks[i].setAttribute('id','link-id'+i);   
+} 
+
+
+
+
