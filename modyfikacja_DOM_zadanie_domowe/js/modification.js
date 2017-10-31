480dd0c44c615eb8a895d51d18b6ea7124bd1f00
Maciej Mucha <mac.muc@wp.pl>
2017-10-31 23:25:43 +0100

    beautification of js file

--- modyfikacja_DOM_zadanie_domowe/js/modification.js
+++ modyfikacja_DOM_zadanie_domowe/js/modification.js
@@ -1,40 +1,33 @@
 'use strict';
 
 
+$(document).ready(function () {
+
+    $("#button1").click(function change() {
+
+        var firstParagraph = $("p").first();
+        var lastParagraph = $("p").last();
+        $(lastParagraph).clone().insertBefore(firstParagraph);
 
 
 
-$(document).ready(function(){
+        $(lastParagraph).remove();
 
-$("#button1").click(function change(){
-   
-    var firstParagraph = $("p").first();
-    var lastParagraph = $("p").last();
-    $(lastParagraph).clone().insertBefore(firstParagraph);
-    
-    
 
-    $(lastParagraph).remove();
-    
-   
-    
-    
-    
-    
-});
-    
-    
-    $("#button2").click(function test(){
-   
-    var firstParagraph = $("p").first();
-    var lastParagraph = $("p").last();
-    $(firstParagraph).clone().insertAfter(lastParagraph);
-    
-    
-
-    $(firstParagraph).remove();
-    
-    
-});
-    
     });
+
+
+    $("#button2").click(function test() {
+
+        var firstParagraph = $("p").first();
+        var lastParagraph = $("p").last();
+        $(firstParagraph).clone().insertAfter(lastParagraph);
+
+
+
+        $(firstParagraph).remove();
+
+
+    });
+
+});
