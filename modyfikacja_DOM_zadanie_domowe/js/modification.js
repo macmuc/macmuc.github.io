483545c50ab2fecf6c35765ab5228b1663fab07a
Maciej Mucha <mac.muc@wp.pl>
2017-11-01 00:33:55 +0100

    poprawki: buttony tworzone za pomocą jquery

--- modyfikacja_DOM_zadanie_domowe/js/modification.js
+++ modyfikacja_DOM_zadanie_domowe/js/modification.js
@@ -3,10 +3,33 @@
 
 $(document).ready(function () {
 
+
+        var firstParagraph = $("p").first();
+        var lastParagraph = $("p").last();
+
+    var newButton = $('<input/>').attr({
+        type: "button",
+        id: "button1",
+        value: "button1",
+
+    });
+  
+    $(newButton).insertBefore(firstParagraph);
+
+    var secondButton = $('<input/>').attr({
+        type: "button",
+        id: "button2",
+        value: "button2",
+
+    });
+   
+    $(secondButton).insertBefore(firstParagraph);
+
     $("#button1").click(function change() {
 
         var firstParagraph = $("p").first();
         var lastParagraph = $("p").last();
+
         $(lastParagraph).clone().insertBefore(firstParagraph);
 
 
@@ -21,6 +44,7 @@ $(document).ready(function () {
 
         var firstParagraph = $("p").first();
         var lastParagraph = $("p").last();
+
         $(firstParagraph).clone().insertAfter(lastParagraph);
 
 
