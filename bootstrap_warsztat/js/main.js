661d551326f5e677431ae2528ff615f32d8e78a7
Maciej Mucha <mac.muc@wp.pl>
2017-11-04 15:59:11 +0100

    dodanie funkcji by menu background nie znikał po odświeżeniu

--- bootstrap_warsztat/js/main.js
+++ bootstrap_warsztat/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
     
-    
+    if($(window).scrollTop()>=50){
+            
+            $("#main-nav").addClass("scroll");}
     
     $(window).scroll(function(){
         if($(window).scrollTop()>=50){
