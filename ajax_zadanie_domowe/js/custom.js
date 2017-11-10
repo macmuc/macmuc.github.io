1487899d1026d7a2a07fa16d558e4892d2935d19
Maciej Mucha <mac.muc@wp.pl>
2017-11-10 12:25:41 +0100

    upiększenie kodu za pomocą beatuify

--- ajax_zadanie_domowe/js/custom.js
+++ ajax_zadanie_domowe/js/custom.js
@@ -5,22 +5,22 @@ $(document).ready(function () {
 
     $("#button1").click(function pobierz() {
 
-        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",function (data) {
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
             console.log(data);
-            
-            
-    var newDiv = $('<div></div>').attr({
-        
-        id: "pobierz-dane",
-        
-    });
-   
-            
-    var newP = $('<p></p>');
-   
+
+
+            var newDiv = $('<div></div>').attr({
+
+                id: "pobierz-dane",
+
+            });
+
+
+            var newP = $('<p></p>');
+
             $("#button1").after(newDiv);
             $(newDiv).append(newP);
-            $(newP).text("imie: " + data.imie +", nazwisko: "+ data.nazwisko + ", zawod: "+ data.zawod +", firma: "+ data.firma)
+            $(newP).text("imie: " + data.imie + ", nazwisko: " + data.nazwisko + ", zawod: " + data.zawod + ", firma: " + data.firma)
         });
 
 
